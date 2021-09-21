b7141d4e615f8551a53b212c4e5819b34bfefbc7
Hany Hamed <h.hamed.elanwar@gmail.com>
2021-09-21 14:36:50 +0300

    [Assignment2] Add more examples

--- Assignments/assignment2/solution/blackbox.h
+++ Assignments/assignment2/solution/blackbox.h
@@ -10,10 +10,26 @@ void blackbox_init(int n) {
 
 double blackbox(double x) {
     switch (__n_method) {
-        case 1:
+        case 1: //2.000000000000
             return 2*x + 1;
-        case 2:
+        case 2: //1.682941969616
             return std::cos(x);
+        case 3: //0.000000000000
+            return std::sin(x);
+        case 4: //0.000000000000
+            return std::tan(x);
+        case 5: //0.666666666667
+            return x*x;
+        case 6: //2.350402387288
+            return std::exp(x);
+        case 7: //0.562774800352
+            return std::log10(x+2);
+        case 8: //1.885618082563
+            return std::sqrt(x+1);
+        case 9: //0.000000000000
+            return x*std::tan(x)*std::tan(x);
+
+
     }
 }
 
